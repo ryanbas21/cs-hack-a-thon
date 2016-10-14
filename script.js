b04b1fd144334f1835ea990fd7897a12aa7cd5e9
ryanbas21 <ryanbas21@gmail.com>
2016-10-13 19:15:37 -0700

    had to update token again

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
-
-function searchByUserName (username) {
-  const self = this;
-  const userData = {};
-  $.ajax({
-    type: "GET",
-    crossDomain:true,
-    url: "https://api.github.com/users/" + username,
-    headers: {"Authorization": "token f50d6704aac96fc91241be735f62e477de3855e8"}
+function searchByUserName(username) {
+    const self = this;
+    const userData = {};
+    $.ajax({
+        type: "GET",
+        crossDomain: true,
+        url: "https://api.github.com/users/" + username,
+        headers: {
+            "Authorization": "token d92c3baa96fc034044518a5837f588deaf0b92e3"
+        }
     }).success((data) => {
-      console.log(data);
-      userData.username = data.login;
-      userData.name = data.name;
-      userData.followers = data.followers;
-      userData.public_repos = data.public_repos;
-      userData.repos_url = data.repos_url;
-      userData.url = data.url;
-      return renderData(userData)
+        console.log(data);
+        userData.username = data.login;
+        userData.name = data.name;
+        userData.followers = data.followers;
+        userData.public_repos = data.public_repos;
+        userData.repos_url = data.repos_url;
+        userData.url = data.url;
+        return renderData(userData)
     });
 }
-function renderData (obj){
-  $('.results').empty();
-  let url = 'http://www.github.com/' + obj['username'];
-  let repos = url + '?tab=repositories';
+function renderData(obj) {
+    $('.results').empty();
+    let url = 'http://www.github.com/' + obj['username'];
+    let repos = url + '?tab=repositories';
     $('.results').append(`<div>Name: ${obj['name']} </div>`);
     $('.results').append(`<div>Followers: ${obj['followers']} </div>`);
     $('.results').append(`<div>Number Of Public Repos: ${obj['public_repos']} </div>`);
     $('.results').append(`<div><a target='_self' href="${url}">GitHub URL</a></div>`);
     $('.results').append(`<div><a target='_self' href="${repos}">Repo Link</a></div>`);
-  }
-
-function checkSelected(){
-  console.log('test');
-  const value = document.getElementById("input").value
-  if (document.getElementById('username').selected){
-    return searchByUserName(value)
-  }
 }
-$(document).ready(function(){
-   $('body').on('click', 'a', function(){
-     chrome.tabs.create({url: $(this).attr('href')});
-     return false;
-   });
+
+function checkSelected() {
+    console.log('test');
+    const value = document.getElementById("input").value
+    if (document.getElementById('username').selected) {
+        return searchByUserName(value)
+    }
+}
+$(document).ready(function() {
+    $('body').on('click', 'a', function() {
+        chrome.tabs.create({url: $(this).attr('href')});
+        return false;
+    });
 });
-document.getElementById('input').addEventListener('keypress', function (e) {
+document.getElementById('input').addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
-    checkSelected();
-  }
+        checkSelected();
+    }
 });
