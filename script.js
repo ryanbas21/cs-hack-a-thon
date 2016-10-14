e7998c29f873c974c8b2bfbe1d871355babc03f7
ryanbas21 <ryanbas21@gmail.com>
2016-10-13 19:13:31 -0700

    slight refactor and fixed structure of code

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ function searchByUserName (username) {
       console.log(data);
       userData.username = data.login;
       userData.name = data.name;
-    //  userData.avatar_url = data.avatar_url
-    //  userData.hirable = data.hirable;
       userData.followers = data.followers;
       userData.public_repos = data.public_repos;
       userData.repos_url = data.repos_url;
@@ -20,29 +18,6 @@ function searchByUserName (username) {
       return renderData(userData)
     });
 }
-// function searchByName (name){
-//   const userData = {};
-//   $.ajax({
-//     type: "GET",
-//     url: "https://api.github.com/search/users?q=" + name,
-//     headers: {"Authorization": "token 089aa30c35d4c877be845f4a04b9976369e08151"}
-//     }).success((data) => {
-//       console.log(data);
-//     userData.avatar_url = data.avatar_url
-//     userData.hirable = data.hirable;
-//     userData.followers = data.followers;
-//     userData.public_repos = data.public_repos;
-//     userData.repo_url = data.repos_url;
-//     userData.url = data.url;
-//     return renderData(userData);
-//   });
-// }
-$(document).ready(function(){
-   $('body').on('click', 'a', function(){
-     chrome.tabs.create({url: $(this).attr('href')});
-     return false;
-   });
-});
 function renderData (obj){
   $('.results').empty();
   let url = 'http://www.github.com/' + obj['username'];
@@ -52,7 +27,6 @@ function renderData (obj){
     $('.results').append(`<div>Number Of Public Repos: ${obj['public_repos']} </div>`);
     $('.results').append(`<div><a target='_self' href="${url}">GitHub URL</a></div>`);
     $('.results').append(`<div><a target='_self' href="${repos}">Repo Link</a></div>`);
-
   }
 
 function checkSelected(){
@@ -62,7 +36,12 @@ function checkSelected(){
     return searchByUserName(value)
   }
 }
-
+$(document).ready(function(){
+   $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
+});
 document.getElementById('input').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
